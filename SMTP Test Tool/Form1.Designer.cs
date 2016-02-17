490b9b125dd55ae713dd1c7b5ef38f25fefe982a
vu-dang <vu.dang@gmail.com>
2016-02-16 22:01:01 -0700

    initial

---
+++ SMTP Test Tool/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace SMTP_Test_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.textBoxAccount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFrom = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxBody = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.checkBoxSSL = new System.Windows.Forms.CheckBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SMTP Server";
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(219, 21);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(299, 20);
+            this.textBoxServer.TabIndex = 1;
+            this.textBoxServer.Text = "smtp.mail.yahoo.com";
+            // 
+            // textBoxAccount
+            // 
+            this.textBoxAccount.Location = new System.Drawing.Point(219, 99);
+            this.textBoxAccount.Name = "textBoxAccount";
+            this.textBoxAccount.Size = new System.Drawing.Size(299, 20);
+            this.textBoxAccount.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "SMTP Account";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(219, 125);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(299, 20);
+            this.textBoxPassword.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // textBoxFrom
+            // 
+            this.textBoxFrom.Location = new System.Drawing.Point(219, 151);
+            this.textBoxFrom.Name = "textBoxFrom";
+            this.textBoxFrom.Size = new System.Drawing.Size(299, 20);
+            this.textBoxFrom.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "From";
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Location = new System.Drawing.Point(219, 177);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(299, 20);
+            this.textBoxTo.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "To";
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Location = new System.Drawing.Point(219, 203);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(299, 20);
+            this.textBoxSubject.TabIndex = 15;
+            this.textBoxSubject.Text = "Hello";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Subject";
+            // 
+            // textBoxBody
+            // 
+            this.textBoxBody.Location = new System.Drawing.Point(219, 229);
+            this.textBoxBody.Name = "textBoxBody";
+            this.textBoxBody.Size = new System.Drawing.Size(299, 20);
+            this.textBoxBody.TabIndex = 17;
+            this.textBoxBody.Text = "It is I";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 232);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Body";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(219, 282);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(86, 32);
+            this.buttonSend.TabIndex = 18;
+            this.buttonSend.Text = "SEND";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(219, 47);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(299, 20);
+            this.textBoxPort.TabIndex = 3;
+            this.textBoxPort.Text = "465";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Port";
+            // 
+            // checkBoxSSL
+            // 
+            this.checkBoxSSL.AutoSize = true;
+            this.checkBoxSSL.Checked = true;
+            this.checkBoxSSL.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxSSL.Location = new System.Drawing.Point(219, 77);
+            this.checkBoxSSL.Name = "checkBoxSSL";
+            this.checkBoxSSL.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxSSL.TabIndex = 5;
+            this.checkBoxSSL.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 77);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "SSL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 337);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.checkBoxSSL);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxBody);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxFrom);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxAccount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxServer);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SMTP Test Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.TextBox textBoxAccount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFrom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxBody;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBoxSSL;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ SMTP Test Tool/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMTP_Test_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            var client = new SmtpClient(textBoxServer.Text, int.Parse(textBoxPort.Text));
+            client.EnableSsl = checkBoxSSL.Checked;
+
+            client.Credentials = new System.Net.NetworkCredential(textBoxAccount.Text, textBoxPassword.Text);
+
+            var message = new MailMessage(textBoxFrom.Text, textBoxTo.Text, textBoxSubject.Text, textBoxBody.Text);
+            MessageBox.Show(string.Format("Sending an e-mail message to {0} by using SMTP host {1} port {2}.",
+         message.To.ToString(), client.Host, client.Port));
+
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                var m = ex.Message;
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                    m += "\n" + ex.Message;
+                }
+
+                MessageBox.Show(m);
+            }
+        }
+    }
+}
